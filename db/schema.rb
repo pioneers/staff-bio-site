9f1842bade7a497dc2160ba944607422d25882da
Matthew Xie <matthewxie@gmail.com>
2018-11-10 15:18:29 -0800

    Added creation date to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108043206) do
+ActiveRecord::Schema.define(version: 20181110222341) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20181108043206) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date_added"
   end
 
 end
